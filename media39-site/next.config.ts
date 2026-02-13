abad58495d08dbe208591927b8b19440b5a952ec
Shahariyar khan <123709357+skhan143@users.noreply.github.com>
2026-02-13 06:47:31 -0600

    new

--- media39-site/next.config.ts
+++ media39-site/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/media39-site",
+  assetPrefix: "/media39-site/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
